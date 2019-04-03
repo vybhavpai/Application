0ef1256950196f162f03bb20d4ef8d97889177e9
raghav0101 <raghavraghuwanshi0101@gmail.com>
2019-04-03 13:07:54 +0530

    back press change

--- app/src/main/java/com/example/vedantmehra/homepage2/HomePage.java
+++ app/src/main/java/com/example/vedantmehra/homepage2/HomePage.java
@@ -441,4 +441,12 @@ public class HomePage extends AppCompatActivity {
         return abdt.onOptionsItemSelected(item) ||  super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
 }
